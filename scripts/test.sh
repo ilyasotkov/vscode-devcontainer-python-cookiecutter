3254c5af5c197419b02b4bf7878e864a3cfe957e
Ilya Sotkov <ilya.sotkov@sanoma.com>
2020-12-06 00:23:36 +0200

    Use matrix build with github actions

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,10 @@ cd $(dirname $0)/..
 
 mkdir -p ./tmp
 cd ./tmp
-cookiecutter .. --no-input --overwrite-if-exists --default-config
+cookiecutter .. \
+    --no-input \
+    --overwrite-if-exists --default-config \
+    python_version=$PYTHON_VERSION
 
 cd $(cat ../cookiecutter.json | jq -r ".project_name")
 ls -alh
